8045f4a9ecbe1ccf935444f435f87e4377460ba5
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-04-14 16:11:16 -0300

    fiex

--- src/resumeData.js
+++ src/resumeData.js
@@ -21,7 +21,7 @@ let resumeData = {
       ],
     "aboutme":"Atualmente curso Sistemas de Informação pela Universidade Federal de Sergipe(UFS)",
     "address":"Brasil",
-    "website":"https://alvarosreis.github.io/",
+    "website":"https://alvarosreis.github.io/portfolio/",
     "education":[
       {
         "UniversityName":"Universidade Federal de Sergipe",
